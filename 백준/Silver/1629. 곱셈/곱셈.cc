82110dbef9dc2d27b7bbc32924eeaae3b1b54d20
Minsu <126066048+levin0303@users.noreply.github.com>
2023-05-08 20:45:21 +0900

    [Silver I] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.cc
+++ 백준/Silver/1629. 곱셈/곱셈.cc
@@ -1,15 +1,16 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 typedef long long ll;
 ll a, b, c;
-ll f(ll x) {
-    if (x == 0)return 1;
-    if (x == 1)return a % c;
-    ll tmp = f(x / 2)%c;
-    if (x % 2 == 0)return (tmp * tmp)%c;
-    if (x % 2 == 1)return ((tmp * tmp)%c * a % c)%c;
-}
-int main(){
-    cin >> a >> b >> c;
-    cout << f(b);
+int main() {
+	cin >> a >> b >> c;
+	ll ans = 1;
+	while (b > 0) {
+		if (b % 2 == 1) {
+			ans = (ans %c* a % c)%c;
+		}
+		a = a * a % c;
+		b /= 2;
+	}
+	cout << ans;
 }
